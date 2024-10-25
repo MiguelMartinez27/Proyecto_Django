9dc02e50e5d21c3552589349b1033670cd8b7368
Miguel Martínez <miguel.martinez.fue@gmail.com>
2024-10-25 04:20:35 -0300

    modelos añadidos por plataforma admin

--- Cats/web/admin.py
+++ Cats/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import rascadores
+
+
+admin.site.register(rascadores)
+
+# admin, admin123
--- Cats/web/models.py
+++ Cats/web/models.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class rascadores(models.Model):
+    rascador_uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name_rascador = models.CharField(max_length=64, null=False, unique=True)
+    descipcion_rascador = models.TextField(blank=True)
+    image_url_rascador = models.URLField()
+    slug_rascador = models.SlugField(max_length=75, unique=True)
+    is_private = models.BooleanField()
